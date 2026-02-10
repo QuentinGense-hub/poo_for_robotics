d992c43ee8d0e33989b3ca86e8ef525b8215d3bf
QuentinGense-hub <quentin.gense@gmail.com>
2026-02-10 16:22:48 +0100

    feat: Rédaction test dans le main

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
+import math
 from robot.robot_mobile import RobotMobile
+from robot.moteur import MoteurDifferentiel, MoteurOmnidirectionnel
 
-robot = RobotMobile(...)
-robot.avancer(1.0)
+# moteur différentiel
+moteur = MoteurDifferentiel()
+robot = RobotMobile(moteur=moteur)
+
+dt = 1.0
+
+robot.afficher()
+robot.commander(v=1.0, omega=math.pi / 4)
+robot.mettre_a_jour(dt)
+robot.afficher()
+
+#moteur omnidirectionnel
+moteur2 = MoteurOmnidirectionnel()
+robot2 = RobotMobile(moteur=moteur2)
+
+robot2.commander(vx=1.0, vy=1.0, omega=0.0)
+robot2.mettre_a_jour(1.0)
+robot2.afficher()
+
+print("Nombre total de robots :", RobotMobile.nombre_robots())
