5f23662a3421e2ed4a2135141f0d52b96309bf69
QuentinGense-hub <quentin.gense@gmail.com>
2026-02-10 15:40:00 +0100

    feat: Ajout class héritage MoteurDifferentiel

--- robot/moteur.py
+++ robot/moteur.py
@@ -11,3 +11,17 @@ class Moteur(ABC):
     def mettre_a_jour(self, robot, dt):
         pass
 
+class MoteurDifferentiel(Moteur):
+    def __init__(self, v=0.0, omega=0.0):
+        self.v = v
+        self.omega = omega
+
+    def commander(self, v, omega):
+        self.v = v
+        self.omega = omega
+
+    def mettre_a_jour(self, robot, dt):
+        robot.orientation += self.omega * dt
+        robot.x += self.v * cos(robot.orientation) * dt
+        robot.y += self.v * sin(robot.orientation) * dt
+
