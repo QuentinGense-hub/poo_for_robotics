d788de31c7e76c0f970ae0d31ec22557a739b3be
QuentinGense-hub <quentin.gense@gmail.com>
2026-02-17 15:42:48 +0100

    feat: modif main.py pour les obstacles

--- main.py
+++ main.py
@@ -3,9 +3,17 @@ from robot.robot_mobile import RobotMobile
 from robot.moteur import MoteurDifferentiel
 from robot.controleur import ControleurClavierPygame
 from robot.vue import VuePygame
+from robot.environnement import Environnement
+from robot.obstacle import ObstacleCirculaire
 
 
 robot = RobotMobile(moteur=MoteurDifferentiel())
+env = Environnement()
+
+env.ajouter_robot(robot)
+env.ajouter_obstacle(ObstacleCirculaire(2, 2, 0.5))
+env.ajouter_obstacle(ObstacleCirculaire(-2, -1, 0.7))
+
 controleur = ControleurClavierPygame()
 vue = VuePygame()
 
@@ -19,11 +27,11 @@ while running:
             running = False
 
     commande = controleur.lire_commande()
-
     robot.commander(**commande)
-    robot.mettre_a_jour(dt)
 
-    vue.dessiner_robot(robot)
+    env.mettre_a_jour(dt)
+
+    vue.dessiner_environnement(env)
     vue.tick(60)
 
 pygame.quit()
