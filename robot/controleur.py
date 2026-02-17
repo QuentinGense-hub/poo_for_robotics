9941dc6f2818e4d0e23bf161e5ec0c3051a493a6
QuentinGense-hub <quentin.gense@gmail.com>
2026-02-17 15:02:32 +0100

    feat: modif controleur.py pour ajouter le controleur clavier

--- robot/controleur.py
+++ robot/controleur.py
@@ -1,3 +1,4 @@
+import pygame
 from abc import ABC, abstractmethod
 
 
@@ -21,3 +22,23 @@ class ControleurTerminal(Controleur):
 
         return {"v": v, "omega": omega}
         
+
+class ControleurClavierPygame(Controleur):
+
+    def lire_commande(self):
+
+        v = 0.0
+        omega = 0.0
+
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_UP]:
+            v = 1.0
+        if keys[pygame.K_DOWN]:
+            v = -1.0
+        if keys[pygame.K_LEFT]:
+            omega = 1.0
+        if keys[pygame.K_RIGHT]:
+            omega = -1.0
+
+        return {"v": v, "omega":omega}
