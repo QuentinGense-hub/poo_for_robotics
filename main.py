4402c54b53ecc55b0700ea1a775bb7d509a5bdc2
QuentinGense-hub <quentin.gense@gmail.com>
2026-02-17 15:03:14 +0100

    feat: modif main.py pour l'utilisation de pygame

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
+import pygame
 from robot.robot_mobile import RobotMobile
 from robot.moteur import MoteurDifferentiel
-from robot.controleur import ControleurTerminal
-from robot.vue import VueTerminal
+from robot.controleur import ControleurClavierPygame
+from robot.vue import VuePygame
 
 
 robot = RobotMobile(moteur=MoteurDifferentiel())
-controleur = ControleurTerminal()
-vue = VueTerminal()
+controleur = ControleurClavierPygame()
+vue = VuePygame()
 
-dt = 1.0
+dt = 0.05
 running = True
 
 while running:
 
-    vue.dessiner_robot(robot)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
     commande = controleur.lire_commande()
 
     robot.commander(**commande)
     robot.mettre_a_jour(dt)
+
+    vue.dessiner_robot(robot)
+    vue.tick(60)
+
+pygame.quit()
